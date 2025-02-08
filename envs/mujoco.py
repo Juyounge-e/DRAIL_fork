f28ee40f0196bec57620fe5f7970aadcf18dde4a
Juyounge-e <ghn09081@gmail.com>
2025-02-08 11:32:54 +0000

    fix: BrokenHalfCheetah entry_point 변경 및 조건 추가

--- envs/mujoco.py
+++ envs/mujoco.py
@@ -5,11 +5,18 @@
 # and ensure the registry entries are pointing at this file as well.
 from .broken_envs import *
 from .target_envs import *
+from gym.envs.mujoco import half_cheetah
 import gym
 
 # HalfCheetahBroken-v2 환경 등록 함수
 def register_halfcheetah_broken_env():
     gym.envs.registration.register(
         id="HalfCheetahBroken-v2",
-        entry_point="envs.target_envs:HalfCheetahModifiedEnv",
+        entry_point="envs.broken_envs:BrokenJoint",
+        max_episode_steps=500,
+        reward_threshold=4800.0,
+        kwargs={
+            'env': half_cheetah.HalfCheetahEnv(),
+            'broken_joint': 0,
+        }
     )
