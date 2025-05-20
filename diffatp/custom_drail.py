f74b8415822ade2742a428ce30fb1f2f3c6988aa
Juyounge-e <ghn09081@gmail.com>
2025-05-21 00:32:57 +0900

    ⚡️ : step +1 보장, 주석 포함

--- diffatp/custom_drail.py
+++ diffatp/custom_drail.py
@@ -63,6 +63,8 @@ class DiffATPDiscriminator(Discriminator):
             t = t.unsqueeze(-1)
         else:
             # 0509 수정 
+            # t = torch.randint(0, n_steps, size=(batch_size//2,)).to(self.args.device)
+            # t = torch.cat([t, n_steps-1-t], dim=0) #[batch_size, 1]
             half_bs = max(batch_size // 2, 1)  # ← 최소 1 보장
             t = torch.randint(0, n_steps, size=(half_bs,), device=self.args.device)
             t = torch.cat([t, n_steps - 1 - t], dim=0)
